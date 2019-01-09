707a3800da8664d4168e41a548fc0c640ee6a514
George Newman <george.newman10@gmail.com>
2019-01-08 18:44:18 -0600

    hd

---
+++ tuts/README.txt
@@ -0,0 +1,3 @@
+t8 shows how to use class to create buttons, etc
+
+t9 and t10 dont display anything, but no errors
---
+++ tuts/t9.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+def doNothing():
+    print('printing nothing')
+
+root = Tk()
+
+menu = Menu(root)
+root.config(menu=menu)
+
+subMenu = Menu(menu)
+menu.add_cascade(label="file", menu=subMenu)
+subMenu.add_command(label="new project...", command=doNothing)
+subMenu.add_command(label="new...", command=doNothing)
+subMenu.add_separator()
+subMenu.add_command(label="Exit", command=doNothing)
+
+editMenu = Menu(menu)
+menu.add_cascade(label="Edit", menu=editMenu)
+editMenu.add_command(label="Redo", command=doNothing)
+
+root.mainloop()
